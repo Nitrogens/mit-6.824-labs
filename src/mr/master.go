c9aa4009a98c2be0a40853a07dfa30ac40f93e24
Nitrogens <1826119316@qq.com>
2020-04-19 00:06:03 +0800

    Completed coding of the Master in MapReduce distributed system

--- src/mr/master.go
+++ src/mr/master.go
@@ -1,18 +1,128 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+	"sync"
+	"time"
+)
 
+type TaskType int
+type StatusType int
+type StageType int
+
+const (
+	kMapTask    TaskType = 0
+	kReduceTask TaskType = 1
+)
+
+const (
+	kStatusCodeOK     StatusType = 0
+	kStatusCodeNoTask StatusType = 1
+	kStatusCodeFinish StatusType = 2
+)
+
+const (
+	kTimeThreshold = time.Second * 10
+)
 
 type Master struct {
 	// Your definitions here.
+	mapTaskList             []Task
+	reduceTaskList          []Task
+	isMapTaskFinished       []bool
+	isReduceTaskFinished    []bool
+	mapTaskChan             chan Task
+	reduceTaskChan          chan Task
+	totalMapTaskCount       int
+	totalReduceTaskCount    int
+	assignedMapTaskCount    int
+	assignedReduceTaskCount int
+	finishedMapTaskCount    int
+	finishedReduceTaskCount int
+	mu                      sync.RWMutex
+}
 
+type Task struct {
+	ID           int
+	Type         TaskType
+	FileNames    []string
+	AssignedTime time.Time
 }
 
 // Your code here -- RPC handlers for the worker to call.
+func (m *Master) GetTask(resp *GetTaskResp) error {
+	var taskInfo Task
+	resp = &GetTaskResp{}
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	if m.assignedMapTaskCount < m.totalMapTaskCount {
+		m.assignedMapTaskCount++
+		taskInfo = <-m.mapTaskChan
+	} else {
+		if m.finishedMapTaskCount < m.totalMapTaskCount {
+			resp.StatusCode = kStatusCodeNoTask
+			resp.TaskInfo = Task{}
+			return nil
+		} else {
+			if m.assignedReduceTaskCount < m.totalReduceTaskCount {
+				m.assignedReduceTaskCount++
+				taskInfo = <-m.reduceTaskChan
+			} else {
+				if m.finishedReduceTaskCount == m.totalReduceTaskCount {
+					resp.StatusCode = kStatusCodeFinish
+				} else {
+					resp.StatusCode = kStatusCodeNoTask
+				}
+				resp.TaskInfo = Task{}
+				return nil
+			}
+		}
+	}
+	taskInfo.AssignedTime = time.Now()
+	resp.StatusCode = kStatusCodeOK
+	resp.TaskInfo = taskInfo
+	return nil
+}
+
+func (m *Master) SetTaskFinished(req *SetTaskFinishedReq, resp *SetTaskFinishedResp) error {
+	resp = &SetTaskFinishedResp{}
+	resp.StatusCode = kStatusCodeOK
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	if req.TaskInfo.Type == kMapTask {
+		m.finishedMapTaskCount++
+		if m.finishedMapTaskCount == m.totalMapTaskCount {
+			close(m.mapTaskChan)
+			for idxReduceTask := 0; idxReduceTask < m.totalReduceTaskCount; idxReduceTask++ {
+				task := Task{
+					ID:        idxReduceTask,
+					Type:      kReduceTask,
+					FileNames: make([]string, m.totalMapTaskCount),
+				}
+				for idxMapTask := 0; idxMapTask < m.totalMapTaskCount; idxMapTask++ {
+					task.FileNames = append(task.FileNames, fmt.Sprintf("mr-%v-%v", idxMapTask, idxReduceTask))
+				}
+				task.AssignedTime = time.Now()
+				m.reduceTaskList = append(m.reduceTaskList, task)
+				m.isReduceTaskFinished[idxReduceTask] = false
+				m.reduceTaskChan <- task
+			}
+		}
+		m.isMapTaskFinished[req.TaskInfo.ID] = true
+	} else if req.TaskInfo.Type == kReduceTask {
+		m.finishedReduceTaskCount++
+		if m.finishedReduceTaskCount == m.totalReduceTaskCount {
+			close(m.reduceTaskChan)
+		}
+		m.isReduceTaskFinished[req.TaskInfo.ID] = true
+	}
+	return nil
+}
 
 //
 // an example RPC handler.
@@ -23,7 +133,6 @@ func (m *Master) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
 }
-
 
 //
 // start a thread that listens for RPCs from worker.go
@@ -49,7 +158,31 @@ func (m *Master) Done() bool {
 	ret := false
 
 	// Your code here.
-
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	if m.finishedReduceTaskCount == m.totalReduceTaskCount {
+		ret = true
+	} else {
+		if m.finishedMapTaskCount < m.totalMapTaskCount {
+			for idx, mapTask := range m.mapTaskList {
+				timeDelta := time.Since(mapTask.AssignedTime)
+				if timeDelta >= kTimeThreshold {
+					m.assignedMapTaskCount--
+					m.mapTaskList[idx].AssignedTime = time.Now()
+					m.mapTaskChan <- m.mapTaskList[idx]
+				}
+			}
+		} else {
+			for idx, reduceTask := range m.reduceTaskList {
+				timeDelta := time.Since(reduceTask.AssignedTime)
+				if timeDelta >= kTimeThreshold {
+					m.assignedReduceTaskCount--
+					m.reduceTaskList[idx].AssignedTime = time.Now()
+					m.reduceTaskChan <- m.mapTaskList[idx]
+				}
+			}
+		}
+	}
 
 	return ret
 }
@@ -63,7 +196,23 @@ func MakeMaster(files []string, nReduce int) *Master {
 	m := Master{}
 
 	// Your code here.
+	m.totalMapTaskCount = len(files)
+	m.totalReduceTaskCount = nReduce
+	m.mapTaskList = make([]Task, 0, m.totalMapTaskCount)
+	m.reduceTaskList = make([]Task, 0, m.totalReduceTaskCount)
+	m.isMapTaskFinished = make([]bool, m.totalMapTaskCount)
+	m.isReduceTaskFinished = make([]bool, m.totalReduceTaskCount)
 
+	for idx, fileName := range files {
+		task := Task{
+			ID:        idx,
+			Type:      kMapTask,
+			FileNames: []string{fileName},
+		}
+		m.mapTaskList = append(m.mapTaskList, task)
+		m.isMapTaskFinished[idx] = false
+		m.mapTaskChan <- task
+	}
 
 	m.server()
 	return &m
--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -23,7 +23,18 @@ type ExampleReply struct {
 }
 
 // Add your RPC definitions here.
+type GetTaskResp struct {
+	StatusCode StatusType
+	TaskInfo   Task
+}
 
+type SetTaskFinishedReq struct {
+	TaskInfo Task
+}
+
+type SetTaskFinishedResp struct {
+	StatusCode StatusType
+}
 
 // Cook up a unique-ish UNIX-domain socket name
 // in /var/tmp, for the master.
